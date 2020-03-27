f1aa57ea4b4eefceb5e16dfd85f85d8500c738a8
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-03-27 18:12:18 -0300

    Adiciona anotação que estava faltando para criar um bean

--- src/main/java/br/com/breadware/configuration/RegistrantApplicationContextAware.java
+++ src/main/java/br/com/breadware/configuration/RegistrantApplicationContextAware.java
@@ -5,7 +5,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RegistrantApplicationContextAware implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
